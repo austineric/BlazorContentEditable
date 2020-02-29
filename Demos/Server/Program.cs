28e0da19a15ef3542b6a604cc2f0b8a375f7f29e
Eric Austin <33876687+austineric@users.noreply.github.com>
2020-02-28 23:26:48 -0600

    Renamed etc

--- Demos/Server/Program.cs
+++ Demos/Server/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace BlazorContentEditable
+namespace Server
 {
     public class Program
     {
