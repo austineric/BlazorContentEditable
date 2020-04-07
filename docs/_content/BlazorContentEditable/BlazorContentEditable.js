b0bb9792bd698edde2c9e7ae0b13338f874c045a
Eric Austin <33876687+austineric@users.noreply.github.com>
2020-04-07 16:31:07 -0500

    Added docs/

---
+++ docs/_content/BlazorContentEditable/BlazorContentEditable.js
@@ -0,0 +1,5 @@
+﻿
+function BlazorContentEditable(DivID, Instance, TextToDisplay) {
+    document.getElementById(DivID).innerHTML = TextToDisplay;
+    document.getElementById(DivID).addEventListener("input", function () { Instance.invokeMethodAsync("GetUpdatedTextFromJavascript", document.getElementById(DivID).innerText) });
+}
