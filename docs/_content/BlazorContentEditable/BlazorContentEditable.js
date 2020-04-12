7bff3c7cda2b5f23156abf5a26d5c9231ce1fefc
Eric Austin <33876687+austineric@users.noreply.github.com>
2020-04-12 07:08:30 -0500

    Removed build files

--- docs/_content/BlazorContentEditable/BlazorContentEditable.js
+++
@@ -1,5 +0,0 @@
-﻿
-function BlazorContentEditable(DivID, Instance, TextToDisplay) {
-    document.getElementById(DivID).innerHTML = TextToDisplay;
-    document.getElementById(DivID).addEventListener("input", function () { Instance.invokeMethodAsync("GetUpdatedTextFromJavascript", document.getElementById(DivID).innerText) });
-}
