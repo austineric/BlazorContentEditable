7d6d5b11849aef1868d8d84dc2e6067c7cf18b85
Eric Austin <33876687+austineric@users.noreply.github.com>
2020-04-17 13:48:16 -0500

    Refactored

--- Projects/BlazorContentEditable/wwwroot/BlazorContentEditable.js
+++ Projects/BlazorContentEditable/wwwroot/BlazorContentEditable.js
@@ -1,5 +1,10 @@
 ﻿
 function BlazorContentEditable(DivID, Instance, TextToDisplay) {
+
+    /*set prefilled text*/
     document.getElementById(DivID).innerHTML = TextToDisplay;
+
+    /*set event listener*/
     document.getElementById(DivID).addEventListener("input", function () { Instance.invokeMethodAsync("GetUpdatedTextFromJavascript", document.getElementById(DivID).innerText) });
+
 }
