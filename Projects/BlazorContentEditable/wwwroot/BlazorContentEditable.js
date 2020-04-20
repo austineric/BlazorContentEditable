3e9b7cfb80f5ffde7a3d9281f013f53da9cf8f64
Eric Austin <33876687+austineric@users.noreply.github.com>
2020-04-20 14:06:02 -0500

    Changed to TextArea

--- Projects/BlazorContentEditable/wwwroot/BlazorContentEditable.js
+++ Projects/BlazorContentEditable/wwwroot/BlazorContentEditable.js
@@ -13,22 +13,23 @@ function BlazorContentEditable(DivID, Instance, TextToDisplay) {
         SetHeight(DivID);
     });
 
-    function SetHeight(DivID) {
+}
 
-        //reset to original height
-        document.getElementById(DivID).style.height = 'auto';
+function SetHeight(DivID) {
 
-        //from https://gomakethings.com/automatically-expand-a-textarea-as-the-user-types-using-vanilla-javascript/
-        // Get the computed styles for the element
-        var computed = window.getComputedStyle(document.getElementById(DivID));
+    //reset to original height
+    document.getElementById(DivID).style.height = 'auto';
 
-        // Calculate the height
-        var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-            + document.getElementById(DivID).scrollHeight
-            + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+    //from https://gomakethings.com/automatically-expand-a-textarea-as-the-user-types-using-vanilla-javascript/
+    // Get the computed styles for the element
+    var computed = window.getComputedStyle(document.getElementById(DivID));
 
-        //set the height
-        document.getElementById(DivID).style.height = (height + 'px');
-    }
+    // Calculate the height
+    var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+        + document.getElementById(DivID).scrollHeight
+        + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+
+    //set the height
+    document.getElementById(DivID).style.height = (height + 'px');
 
 }
